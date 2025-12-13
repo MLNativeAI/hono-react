ba08e8e98c7e8326aaad51b472ee6281f7ae67f7
Lukasz Myslinski <lukasz@mlnative.com>
2025-12-13 19:16:04 +0100

    queries cleanup

--- packages/db/src/types/api-keys.ts
+++
@@ -1,9 +0,0 @@
-export type ApiKey = {
-  id: string;
-  name: string | null;
-  key: string;
-  userId: string;
-  enabled: boolean;
-  createdAt: string;
-  lastRequest: string | null;
-};
--- packages/db/types.ts
+++ packages/db/types.ts
@@ -1,5 +1,1 @@
-export * from "./src/types/api-keys";
 export * from "./src/types/configuration";
-export * from "./src/types/executions";
-export * from "./src/types/tables";
-export * from "./src/types/workflow-config";
